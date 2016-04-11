19511d5799987e618da468666f13391ae8411a2a
Max Maischein <corion@corion.net>
2016-04-11 21:27:35 +0200

    Add the most rudimentary documentation

--- lib/Archive/SevenZip/API/ArchiveTar.pm
+++ lib/Archive/SevenZip/API/ArchiveTar.pm
@@ -1,6 +1,7 @@
 package Archive::SevenZip::API::ArchiveTar;
 use strict;
 use Archive::SevenZip;
+use Carp 'croak';
 
 use vars qw($VERSION);
 $VERSION= '0.06';
@@ -23,6 +24,8 @@ API to make extracting work. Ideally
 use this API to enable a script that uses Archive::Tar
 to also read other archive files supported by 7z.
 
+=head1 METHODS
+
 =cut
 
 sub new {
@@ -33,15 +36,27 @@ sub new {
 
 sub sevenZip { $_[0]->{sevenZip} }
 
+=head2 C<< ->contains_file >>
+
+=cut
+
 sub contains_file {
     my( $self, $name ) = @_;
     $self->sevenZip->memmberNamed( $name )
 };
 
+=head2 C<< ->get_content >>
+
+=cut
+
 sub get_content {
     my( $self, $name ) = @_;
     $self->sevenZip->content( $name );
 };
+
+=head2 C<< ->list_files >>
+
+=cut
 
 sub list_files {
     my ($self,$properties) = @_;
@@ -51,10 +66,43 @@ sub list_files {
     map { $_->fileName } @files
 }
 
+=head2 C<< ->extract_file >>
+
+=cut
+
 sub extract_file {
     my ($self,$file,$target) = @_;
     $self->sevenZip->extractMember( $file => $target );
 };
 
-
 1;
+
+=head1 REPOSITORY
+
+The public repository of this module is 
+L<http://github.com/Corion/archive-sevenzip>.
+
+=head1 SUPPORT
+
+The public support forum of this module is
+L<https://perlmonks.org/>.
+
+=head1 BUG TRACKER
+
+Please report bugs in this module via the RT CPAN bug queue at
+L<https://rt.cpan.org/Public/Dist/Display.html?Name=Archive-SevenZip>
+or via mail to L<archive-sevenzip-Bugs@rt.cpan.org>.
+
+=head1 AUTHOR
+
+Max Maischein C<corion@cpan.org>
+
+=head1 COPYRIGHT (c)
+
+Copyright 2015-2016 by Max Maischein C<corion@cpan.org>.
+
+=head1 LICENSE
+
+This module is released under the same terms as Perl itself.
+
+=cut
